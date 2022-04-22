3b5ddbe545827025c76a2a10394af461a989ef19
ETHAN STRAIN <ETHAN.STRAIN@otech.edu>
2022-04-22 14:57:06 -0600

    images

--- bubbaspainters/src/components/NavBar.vue
+++ bubbaspainters/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav>
+    <nav >
         <v-toolbar flat app>
             <v-app-bar-nav-icon @click="menu = !menu"></v-app-bar-nav-icon>
             <v-toolbar-title class="text-uppercase gray--text">
@@ -8,9 +8,10 @@
             </v-toolbar-title>
             <v-spacer></v-spacer>
             <span ><router-link to="/contact" class="text-decoration-none">Contact Us</router-link></span>
-            <v-navigation-drawer app v-model="menu">
-            <v-list>
-        <v-list-subheader >Menu</v-list-subheader>
+        <v-navigation-drawer app v-model="menu" >
+        <v-list class="text-center">
+        <v-img src='../images/logo.png'></v-img>
+        <v-list-subheader class="text-h3 text-decoration-underline">Menu</v-list-subheader>
         <v-list-item
           v-for="(link, i) in links"
           :key="i"
@@ -20,7 +21,7 @@
           router :to="link.route"
         >
           <v-list-item-avatar start>
-            <v-icon :icon="link.icon"></v-icon>
+            <v-icon :icon="link.icon" flat app></v-icon>
           </v-list-item-avatar>
           <v-list-item-title v-text="link.text" @click="menu = !menu"></v-list-item-title>
         </v-list-item>
@@ -40,6 +41,8 @@ export default {
             links:[
                 {icon: 'mdi-home', text: 'Home', route: '/'},
                 {icon: 'mdi-account-tie', text: 'About Bubbas', route: '/about'},
+                {icon: 'mdi-briefcase-outline', text: 'Our Work', route: '/work'},
+                {icon: 'mdi-account-box', text: 'Contact Us', route: '/contact'},
             ]
         }
     }
