053854be5770702b5702672c4a4e4a37980f6c19
ETHAN STRAIN <ETHAN.STRAIN@otech.edu>
2022-04-22 14:14:29 -0600

    2500 init

---
+++ bubbaspainters/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <nav-bar/>
+
+    <v-content> <router-view/> </v-content>
+  </v-app>
+</template>
+
+<script>
+// import HomeView from './views/HomeView.vue'
+// import MyWork from './views/MyWork.vue'
+// import ContactView from './views/ContactView.vue' 
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+  components:{
+    NavBar,
+  }
+}
+</script>
---
+++ bubbaspainters/src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-app-bar-nav-icon @click="menu = !menu"></v-app-bar-nav-icon>
+            <v-toolbar-title class="text-uppercase gray--text">
+                <span>Bubba's</span>
+                <span class="font-weight-light">Painting</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <span ><router-link to="/contact" class="text-decoration-none">Contact Us</router-link></span>
+            <v-navigation-drawer app v-model="menu">
+            <v-list>
+        <v-list-subheader >Menu</v-list-subheader>
+        <v-list-item
+          v-for="(link, i) in links"
+          :key="i"
+          :value="item"
+          active-color="primary"
+          variant="plain"
+          router :to="link.route"
+        >
+          <v-list-item-avatar start>
+            <v-icon :icon="link.icon"></v-icon>
+          </v-list-item-avatar>
+          <v-list-item-title v-text="link.text" @click="menu = !menu"></v-list-item-title>
+        </v-list-item>
+      </v-list>
+        </v-navigation-drawer>
+        </v-toolbar>
+        
+    </nav>
+</template>
+<script>
+
+
+export default {
+    data(){
+        return{
+            menu: false,
+            links:[
+                {icon: 'mdi-home', text: 'Home', route: '/'},
+                {icon: 'mdi-account-tie', text: 'About Bubbas', route: '/about'},
+            ]
+        }
+    }
+}
+</script>
